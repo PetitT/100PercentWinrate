444ef6c5c893d06e2ba683b76633e28094abf77f
PetitT <xcthib@hotmail.com>
2020-04-27 03:34:46 +0200

    Honestly don't know what I did but it kinda works

--- 100%WINRATE/Assets/Scripts/Character/PlayerHealth.cs
+++ 100%WINRATE/Assets/Scripts/Character/PlayerHealth.cs
@@ -35,11 +35,6 @@ public class PlayerHealth : MonoBehaviourPun
         {
             Regenerate();
         }
-
-        if (Input.GetKeyDown(KeyCode.KeypadEnter))
-        {
-            onDeath?.Invoke();
-        }
     }
 
     private void OnDisable()
--- 100%WINRATE/Assets/Scripts/Character/PlayerHealthDisplay.cs
+++ 100%WINRATE/Assets/Scripts/Character/PlayerHealthDisplay.cs
@@ -6,18 +6,24 @@ using UnityEngine.UI;
 
 public class PlayerHealthDisplay : MonoBehaviourPun
 {
-    [SerializeField] private GameObject healthBar;
-    private float YMaxSize;
+    [SerializeField] private SpriteRenderer healthBar;
+    private int ID;
 
-    private void Awake()
+    private void Start()
     {
-        YMaxSize = healthBar.transform.localScale.y;
+        ID = healthBar.GetComponent<PhotonView>().ViewID;
     }
 
     public void UpdateHealth(float currentHealth, float maxHealth)
     {
         float normalizedHealth = currentHealth / maxHealth;
-        float newSize = YMaxSize * normalizedHealth;
-        healthBar.transform.localScale = new Vector2(healthBar.transform.localScale.x, newSize);
+        photonView.RPC("DisplayHealth", RpcTarget.AllBuffered, ID, normalizedHealth);
+    }
+
+    [PunRPC]
+    private void DisplayHealth(int ID, float normalizedHealth)
+    {
+        Color newColor = new Color(Color.white.r, Color.white.g, Color.white.b, normalizedHealth);
+        healthBar.material.SetColor("_BaseColor", newColor);
     }
 }
